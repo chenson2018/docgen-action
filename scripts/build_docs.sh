be0cd70487357aa0cc97937ed35851da772968b6
Chris Henson <chrishenson.net@gmail.com>
2025-11-17 08:28:28 -0500

    just echo the references file

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -80,10 +80,7 @@ EOF
 cd docbuild
 
 # Place references.bib in the location expected by doc-gen4
-if [ -f ../../$REFERENCES ]; then
-  mkdir docs
-  cp ../../$REFERENCES ./docs/references.bib
-fi
+echo $REFERENCES
 
 # Disable an error message due to a non-blocking bug. See Zulip
 MATHLIB_NO_CACHE_ON_UPDATE=1 ~/.elan/bin/lake update $NAME
